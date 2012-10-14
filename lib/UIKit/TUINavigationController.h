a83695e8b1bb7c9b0f54324f4c62c67a8c94d81a
Robert <devteam.codafi@gmail.com>
2012-10-13 21:11:26 -0600

    Uncrustified.

--- lib/UIKit/TUINavigationController.h
+++ lib/UIKit/TUINavigationController.h
@@ -13,36 +13,36 @@
 #import <CoreGraphics/CoreGraphics.h>
 
 /*!
- TUINavigationController manages a stack of view controllers and a navigation bar.
- It performs horizontal view transitions for pushed and popped views while keeping the navigation bar in sync.
+   TUINavigationController manages a stack of view controllers and a navigation bar.
+   It performs horizontal view transitions for pushed and popped views while keeping the navigation bar in sync.
  */
 
 //Custom animation block for TUINavigationController's -pushViewController:withAnimationBlock;
 //Animates all methods called within it for the default duration of a push animation, then cleans up after itself
 //Do not call view removal methods on any of the block's parameters, they will be performed for you at the end of animation
-typedef void (^TUINavigationAnimationBlock)(TUIViewController *pushingFrom, TUIViewController *pushingTo, CGRect suggestedFrame);
+typedef void ( ^TUINavigationAnimationBlock)(TUIViewController * pushingFrom, TUIViewController * pushingTo, CGRect suggestedFrame);
 
 @protocol TUINavigationControllerDelegate;
 
 @interface TUINavigationController : TUIViewController
 
-- (id)initWithRootViewController:(TUIViewController *)rootViewController; // Convenience method pushes the root view controller without animation.
+- (id) initWithRootViewController:(TUIViewController *)rootViewController; // Convenience method pushes the root view controller without animation.
 
-- (void)pushViewController:(TUIViewController *)viewController animated:(BOOL)animated; // Uses a horizontal slide transition. Has no effect if the view controller is already in the stack.
+- (void) pushViewController:(TUIViewController *)viewController animated:(BOOL)animated; // Uses a horizontal slide transition. Has no effect if the view controller is already in the stack.
 
-- (void)pushViewController:(TUIViewController *)viewController withAnimationBlock:(TUINavigationAnimationBlock)block; // Uses a pre-defined block transition. Has no effect if the view controller is already in the stack.
+- (void) pushViewController:(TUIViewController *)viewController withAnimationBlock:(TUINavigationAnimationBlock)block; // Uses a pre-defined block transition. Has no effect if the view controller is already in the stack.
 
-- (TUIViewController *)popViewControllerAnimated:(BOOL)animated; // Returns the popped controller.
-- (NSArray *)popToViewController:(TUIViewController *)viewController animated:(BOOL)animated; // Pops view controllers until the one specified is on top. Returns the popped controllers.
-- (NSArray *)popToRootViewControllerAnimated:(BOOL)animated; // Pops until there's only a single view controller left on the stack. Returns the popped controllers.
+- (TUIViewController *) popViewControllerAnimated:(BOOL)animated; // Returns the popped controller.
+- (NSArray *)           popToViewController:(TUIViewController *)viewController animated:(BOOL)animated; // Pops view controllers until the one specified is on top. Returns the popped controllers.
+- (NSArray *)           popToRootViewControllerAnimated:(BOOL)animated; // Pops until there's only a single view controller left on the stack. Returns the popped controllers.
 
-@property(nonatomic,readonly,retain) TUIViewController *topViewController; // The top view controller on the stack.
-@property(nonatomic,readonly,retain) TUIViewController *visibleViewController; // Return modal view controller if it exists. Otherwise the top view controller.
+@property (nonatomic, readonly, retain) TUIViewController *topViewController; // The top view controller on the stack.
+@property (nonatomic, readonly, retain) TUIViewController *visibleViewController; // Return modal view controller if it exists. Otherwise the top view controller.
 
-@property (nonatomic,strong) NSMutableArray *viewControllers; // The current view controller stack.
-- (void)setViewControllers:(NSArray *)viewControllers animated:(BOOL)animated; // If animated is YES, then simulate a push or pop depending on whether the new top view controller was previously in the stack.
+@property (nonatomic, strong) NSMutableArray *viewControllers; // The current view controller stack.
+- (void) setViewControllers:(NSArray *)viewControllers animated:(BOOL)animated; // If animated is YES, then simulate a push or pop depending on whether the new top view controller was previously in the stack.
 
-@property(nonatomic, assign) id<TUINavigationControllerDelegate> delegate;
+@property (nonatomic, assign) id <TUINavigationControllerDelegate> delegate;
 
 @end
 
@@ -51,13 +51,13 @@ typedef void (^TUINavigationAnimationBlock)(TUIViewController *pushingFrom, TUIV
 @optional
 
 // Called when the navigation controller shows a new top view controller via a push, pop or setting of the view controller stack.
-- (void)navigationController:(TUINavigationController *)navigationController willShowViewController:(TUIViewController *)viewController animated:(BOOL)animated;
-- (void)navigationController:(TUINavigationController *)navigationController didShowViewController:(TUIViewController *)viewController animated:(BOOL)animated;
+- (void) navigationController:(TUINavigationController *)navigationController willShowViewController:(TUIViewController *)viewController animated:(BOOL)animated;
+- (void) navigationController:(TUINavigationController *)navigationController didShowViewController:(TUIViewController *)viewController animated:(BOOL)animated;
 
 @end
 
 @interface TUIViewController (TUINavigationControllerItem)
 
-@property(nonatomic,readonly,retain) TUINavigationController *navigationController; // If this view controller has been pushed onto a navigation controller, return it.
+@property (nonatomic, readonly, retain) TUINavigationController *navigationController; // If this view controller has been pushed onto a navigation controller, return it.
 
 @end
--- lib/UIKit/TUINavigationController.m
+++ lib/UIKit/TUINavigationController.m
@@ -30,27 +30,27 @@ static const NSTimeInterval kAnimationDuration = 0.5;
 
 - (id)initWithRootViewController:(TUIViewController *)rootViewController {
 	if ((self=[super init])) {
-        self.viewControllers = [NSMutableArray arrayWithObject:rootViewController];
+		self.viewControllers = [NSMutableArray arrayWithObject:rootViewController];
 		[rootViewController setParentViewController:self];
 		[self setVisibleViewController:rootViewController];
-    }
-    return self;
+	}
+	return self;
 }
 
 - (void)loadView
 {
-    self.view = [[TUIView alloc] initWithFrame:CGRectMake(0,0,320,480)];
+	self.view = [[TUIView alloc] initWithFrame:CGRectMake(0,0,320,480)];
 	[self.view setDrawRect:^(TUIView *v, CGRect rect) {
 		[[NSColor clearColor]set];
 		NSRectFill(rect);
 	}];
 	[self.view setAutoresizingMask:TUIViewAutoresizingFlexibleSize];
-    
-    TUIViewController *viewController = self.visibleViewController;
-    viewController.view.frame = [self _controllerFrameForTransition:_TUINavigationControllerVisibleControllerTransitionNone];
-    viewController.view.autoresizingMask = TUIViewAutoresizingFlexibleSize;
 	
-    [self.view addSubview:viewController.view];
+	TUIViewController *viewController = self.visibleViewController;
+	viewController.view.frame = [self _controllerFrameForTransition:_TUINavigationControllerVisibleControllerTransitionNone];
+	viewController.view.autoresizingMask = TUIViewAutoresizingFlexibleSize;
+	
+	[self.view addSubview:viewController.view];
 }
 
 -(TUIViewController*)topViewController {
@@ -59,89 +59,89 @@ static const NSTimeInterval kAnimationDuration = 0.5;
 
 - (void)viewWillAppear:(BOOL)animated
 {
-    [super viewWillAppear:animated];
-    [self.visibleViewController viewWillAppear:animated];
+	[super viewWillAppear:animated];
+	[self.visibleViewController viewWillAppear:animated];
 }
 
 - (void)viewDidAppear:(BOOL)animated
 {
-    [super viewDidAppear:animated];
-    [self.visibleViewController viewDidAppear:animated];
+	[super viewDidAppear:animated];
+	[self.visibleViewController viewDidAppear:animated];
 }
 
 - (void)viewWillDisappear:(BOOL)animated
 {
-    [super viewWillDisappear:animated];
-    [self.visibleViewController viewWillDisappear:animated];
+	[super viewWillDisappear:animated];
+	[self.visibleViewController viewWillDisappear:animated];
 }
 
 - (void)viewDidDisappear:(BOOL)animated
 {
-    [super viewDidDisappear:animated];
-    [self.visibleViewController viewDidDisappear:animated];
+	[super viewDidDisappear:animated];
+	[self.visibleViewController viewDidDisappear:animated];
 }
 
 - (CGRect)_controllerFrameForTransition:(_TUINavigationControllerVisibleControllerTransition)transition
 {
-    CGRect controllerFrame = self.view.bounds;
-    
-    if (transition == _TUINavigationControllerVisibleControllerTransitionPushAnimated) {
-        controllerFrame = CGRectOffset(controllerFrame, controllerFrame.size.width, 0);
-    } else if (transition == _TUINavigationControllerVisibleControllerTransitionPopAnimated) {
-        controllerFrame = CGRectOffset(controllerFrame, -controllerFrame.size.width, 0);
-    }
-    
-    return controllerFrame;
+	CGRect controllerFrame = self.view.bounds;
+	
+	if (transition == _TUINavigationControllerVisibleControllerTransitionPushAnimated) {
+		controllerFrame = CGRectOffset(controllerFrame, controllerFrame.size.width, 0);
+	} else if (transition == _TUINavigationControllerVisibleControllerTransitionPopAnimated) {
+		controllerFrame = CGRectOffset(controllerFrame, -controllerFrame.size.width, 0);
+	}
+	
+	return controllerFrame;
 }
 
 - (void)pushViewController:(TUIViewController *)viewController animated:(BOOL)animated
 {
-    assert(![_viewControllers containsObject:viewController]);
+	assert(![_viewControllers containsObject:viewController]);
 	
-    // override the animated property based on current state
-    animated = animated && _visibleViewController;
-    
+	// override the animated property based on current state
+	animated = animated && _visibleViewController;
+	
 	[viewController.view setAutoresizingMask:TUIViewAutoresizingFlexibleSize];
 	
-    // push on to controllers stack
-    [_viewControllers addObject:viewController];
-    
-    // take ownership responsibility
-    [viewController setParentViewController:self];
-    
+	// push on to controllers stack
+	[_viewControllers addObject:viewController];
+	
+	// take ownership responsibility
+	[viewController setParentViewController:self];
+	
 	// if animated and on screen, begin part of the transition immediately, specifically, get the new view
-    // on screen asap and tell the new controller it's about to be made visible in an animated fashion
+	// on screen asap and tell the new controller it's about to be made visible in an animated fashion
 	if (animated) {
 		_visibleViewControllerTransition = _TUINavigationControllerVisibleControllerTransitionPushAnimated;
 		
 		viewController.view.frame = [self _controllerFrameForTransition:_visibleViewControllerTransition];
-        
+		
 		[_visibleViewController viewWillDisappear:YES];
 		[viewController viewWillAppear:YES];
-        
-        if ([_delegate respondsToSelector:@selector(navigationController:willShowViewController:animated:)]) {
-            [_delegate navigationController:self willShowViewController:viewController animated:YES];
-        }
+		
+		if ([_delegate respondsToSelector:@selector(navigationController:willShowViewController:animated:)]) {
+			[_delegate navigationController:self willShowViewController:viewController animated:YES];
+		}
 		
 		[self.view insertSubview:viewController.view atIndex:0];
 	}
-    
+	
 	[self _setVisibleViewControllerNeedsUpdate];
 }
 
 - (void)pushViewController:(TUIViewController *)viewController withAnimationBlock:(TUINavigationAnimationBlock)block {
 	assert(![_viewControllers containsObject:viewController]);
-    
+	
 	[viewController.view setAutoresizingMask:TUIViewAutoresizingFlexibleSize];
 	
-    // push on to controllers stack
-    [_viewControllers addObject:viewController];
-    
-    // take ownership responsibility
-    [viewController setParentViewController:self];
-    
+	// push on to controllers stack
+	[_viewControllers addObject:viewController];
+	
+	// take ownership responsibility
+	[viewController setParentViewController:self];
+	
 	// if animated and on screen, begin part of the transition immediately, specifically, get the new view
-    // on screen asap and tell the new controller it's about to be made visible in an animated fashion
+	// on screen asap and tell the new controller it's about to be made visible in an animated fashion
 	_visibleViewControllerTransition = _TUINavigationControllerVisibleControllerTransitionPushAnimated;
 	
 	viewController.view.frame = [self _controllerFrameForTransition:_visibleViewControllerTransition];
@@ -154,31 +154,31 @@ static const NSTimeInterval kAnimationDuration = 0.5;
 	}
 	
 	[self.view insertSubview:viewController.view atIndex:0];
-    
+	
 	[self _setVisibleViewControllerNeedsUpdateWithBlock:block];
 }
 
 - (TUIViewController *)popViewControllerAnimated:(BOOL)animated
 {
-    // don't allow popping the rootViewController
-    if ([_viewControllers count] <= 1) {
-        return nil;
-    }
-    
-    TUIViewController *formerTopViewController = self.topViewController;
+	// don't allow popping the rootViewController
+	if ([_viewControllers count] <= 1) {
+		return nil;
+	}
+	
+	TUIViewController *formerTopViewController = self.topViewController;
 	
 	
 	// pop the controller stack
-    [_viewControllers removeLastObject];
-    
-    // give up ownership of the view controller
-    [formerTopViewController setParentViewController:nil];
-    
+	[_viewControllers removeLastObject];
+	
+	// give up ownership of the view controller
+	[formerTopViewController setParentViewController:nil];
+	
 	// if animated, begin part of the transition immediately, specifically, get the new top view on screen asap
 	// and tell the old visible controller it's about to be disappeared in an animated fashion
 	if (animated) {
-        // note the new top here so we don't have to use the accessor method all the time
-        TUIViewController *topController = self.topViewController;
+		// note the new top here so we don't have to use the accessor method all the time
+		TUIViewController *topController = self.topViewController;
 		
 		_visibleViewControllerTransition = _TUINavigationControllerVisibleControllerTransitionPopAnimated;
 		
@@ -189,20 +189,20 @@ static const NSTimeInterval kAnimationDuration = 0.5;
 			_visibleViewController.view.frame = [self _controllerFrameForTransition:_TUINavigationControllerVisibleControllerTransitionNone];
 			[self.view insertSubview:_visibleViewController.view atIndex:0];
 		}
-        
+		
 		topController.view.frame = [self _controllerFrameForTransition:_visibleViewControllerTransition];
-        
+		
 		[_visibleViewController viewWillDisappear:YES];
 		[topController viewWillAppear:YES];
 		
-        if ([_delegate respondsToSelector:@selector(navigationController:willShowViewController:animated:)]) {
-            [_delegate navigationController:self willShowViewController:topController animated:YES];
-        }
+		if ([_delegate respondsToSelector:@selector(navigationController:willShowViewController:animated:)]) {
+			[_delegate navigationController:self willShowViewController:topController animated:YES];
+		}
 		
 		[self.view insertSubview:topController.view atIndex:0];
 		
 	}
-    
+	
 	[self _setVisibleViewControllerNeedsUpdate];
 	
 	return formerTopViewController;
@@ -212,40 +212,40 @@ static const NSTimeInterval kAnimationDuration = 0.5;
 
 - (NSArray *)popToViewController:(TUIViewController *)viewController animated:(BOOL)animated
 {
-    NSMutableArray *popped = [[NSMutableArray alloc] init];
+	NSMutableArray *popped = [[NSMutableArray alloc] init];
 	
-    if ([_viewControllers containsObject:viewController]) {
-        while (self.topViewController != viewController) {
-            TUIViewController *poppedController = [self popViewControllerAnimated:animated];
-            if (poppedController) {
-                [popped addObject:poppedController];
-            } else {
-                break;
-            }
-        }
-    }
-    
-    return [popped copy];
+	if ([_viewControllers containsObject:viewController]) {
+		while (self.topViewController != viewController) {
+			TUIViewController *poppedController = [self popViewControllerAnimated:animated];
+			if (poppedController) {
+				[popped addObject:poppedController];
+			} else {
+				break;
+			}
+		}
+	}
+	
+	return [popped copy];
 }
 
 - (NSArray *)popToRootViewControllerAnimated:(BOOL)animated
 {
-    return [self popToViewController:[_viewControllers objectAtIndex:0] animated:animated];
+	return [self popToViewController:[_viewControllers objectAtIndex:0] animated:animated];
 }
 
 - (void)setViewControllers:(NSArray *)newViewControllers animated:(BOOL)animated {
 	assert([newViewControllers count] >= 1);
 	
-    if (![newViewControllers isEqualToArray:_viewControllers]) {
-        // remove them all in bulk
-        [_viewControllers makeObjectsPerformSelector:@selector(_setParentViewController:) withObject:nil];
-        [_viewControllers removeAllObjects];
-        
-        // add them back in one-by-one and only apply animation to the last one (if any)
-        for (TUIViewController *controller in newViewControllers) {
-            [self pushViewController:controller animated:(animated && (controller == [newViewControllers lastObject]))];
-        }
-    }
+	if (![newViewControllers isEqualToArray:_viewControllers]) {
+		// remove them all in bulk
+		[_viewControllers makeObjectsPerformSelector:@selector(_setParentViewController:) withObject:nil];
+		[_viewControllers removeAllObjects];
+		
+		// add them back in one-by-one and only apply animation to the last one (if any)
+		for (TUIViewController *controller in newViewControllers) {
+			[self pushViewController:controller animated:(animated && (controller == [newViewControllers lastObject]))];
+		}
+	}
 }
 
 - (void)_setVisibleViewControllerNeedsUpdate
@@ -270,29 +270,29 @@ static const NSTimeInterval kAnimationDuration = 0.5;
 {
 	// do some bookkeeping
 	_visibleViewControllerNeedsUpdate = NO;
-    TUIViewController *topViewController = self.topViewController;
-    
+	TUIViewController *topViewController = self.topViewController;
+	
 	// make sure the new top view is both loaded and set to appear in the correct place
 	topViewController.view.frame = [self _controllerFrameForTransition:_visibleViewControllerTransition];
-    
+	
 	if (_visibleViewControllerTransition == _TUINavigationControllerVisibleControllerTransitionNone) {
 		[_visibleViewController viewWillDisappear:NO];
 		[topViewController viewWillAppear:NO];
-        
-        if ([_delegate respondsToSelector:@selector(navigationController:willShowViewController:animated:)]) {
-            [_delegate navigationController:self willShowViewController:topViewController animated:NO];
-        }
-        
+		
+		if ([_delegate respondsToSelector:@selector(navigationController:willShowViewController:animated:)]) {
+			[_delegate navigationController:self willShowViewController:topViewController animated:NO];
+		}
+		
 		[_visibleViewController.view removeFromSuperview];
 		[self.view insertSubview:topViewController.view atIndex:0];
-        
+		
 		[_visibleViewController viewDidDisappear:NO];
 		[topViewController viewDidAppear:NO];
 		
-        if ([_delegate respondsToSelector:@selector(navigationController:didShowViewController:animated:)]) {
-            [_delegate navigationController:self didShowViewController:topViewController animated:NO];
-        }
-    } else {
+		if ([_delegate respondsToSelector:@selector(navigationController:didShowViewController:animated:)]) {
+			[_delegate navigationController:self didShowViewController:topViewController animated:NO];
+		}
+	} else {
 		if (block) {
 			TUIViewController *previouslyVisibleViewController = _visibleViewController;
 			const CGRect topControllerFrame = [self _controllerFrameForTransition:_TUINavigationControllerVisibleControllerTransitionNone];
@@ -332,9 +332,9 @@ static const NSTimeInterval kAnimationDuration = 0.5;
 								  }
 							  }];
 		}
-    }
+	}
 	_visibleViewController = topViewController;
-    
+	
 }
 
 @end
