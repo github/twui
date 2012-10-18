259e1ec2d661b3d66648a2d13c1fcb6873d28f9d
Max Goedjen <max.goedjen@gmail.com>
2012-10-17 20:41:45 -0700

    Fixed #113 (TUITableView doesn't move section headers when moving)

--- lib/UIKit/TUITableView.m
+++ lib/UIKit/TUITableView.m
@@ -765,8 +765,10 @@ static NSInteger SortCells(TUITableViewCell *a, TUITableViewCell *b, void *ctx)
 					}
 				}
 				
-				section.headerView.frame = headerFrame;
-				[section.headerView setNeedsLayout];
+                section.headerView.frame = headerFrame;
+                if (![self __isDraggingCell]) {
+                    [section.headerView setNeedsLayout];
+                }
 				
 				if(section.headerView.superview == nil){
 					[self addSubview:section.headerView];
